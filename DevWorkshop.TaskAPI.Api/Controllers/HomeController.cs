eeca4870ea82706fdaa1668095fee91bb051f37b
Parcek87 <miguecab8715@gmail.com>
2025-08-12 21:05:59 -0500

    commit1 Tallerguiado

---
+++ DevWorkshop.TaskAPI.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevWorkshop.TaskAPI.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
